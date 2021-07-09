780cb7c415d4d900dee4932c48d282f30d9d2095
swapshivam3 <swapshivam3@gmail.com>
2021-07-09 15:01:51 +0530

    Nope

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css'
+import CustomLayout from './Containers/Layout';
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout/>
+    </div>
+  );
+}
+
+export default App;
